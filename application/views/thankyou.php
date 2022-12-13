79358204a7d6d4c8040c2c158aa52666716690df
Christopher John Gamo <christopherjohngamo@gmail.com>
2022-12-14 01:37:41 +0800

    Update

--- application/views/thankyou.php
+++ application/views/thankyou.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/intakeform.css">
+    <link rel="stylesheet" href="<?=base_url()?>/assets/css/intakeform.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
     </style>
