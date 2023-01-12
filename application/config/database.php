6f887010aaa33d8d417d83951b0833c2ded76017
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-01-13 06:35:45 +0800

    Update

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '45.79.199.169',
+	'hostname' => '139.144.29.38',
 	'username' => 'shinemmgnew',
 	'password' => '5h!n3wwg_dr5',
 	'database' => 'mobile_drs',
