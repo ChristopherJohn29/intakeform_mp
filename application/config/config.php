6e741186f10dd0238827feb254e89acf5a13699a
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-01-19 00:12:51 +0800

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.105.155.128/';
+$config['base_url'] = 'http://appointment.themobiledrs.com/';
 
 /*
 |--------------------------------------------------------------------------
