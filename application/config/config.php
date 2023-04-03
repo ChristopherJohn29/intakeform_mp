14c2248010b6165c84f39ab505387cbd28552c0d
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-04-04 00:55:33 +0800

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://139.144.254.143/';
+$config['base_url'] = 'https://appointment.physicianmedicalgroup.com';
 
 /*
 |--------------------------------------------------------------------------
