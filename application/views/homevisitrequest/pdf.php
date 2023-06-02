1e7741f6bd6fd8c0b76d29f5408a8cd82530ac6a
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-06-02 16:59:05 +0800

    Debug

--- application/views/homevisitrequest/pdf.php
+++ application/views/homevisitrequest/pdf.php
@@ -217,7 +217,7 @@
                </td>
             </tr>
             <tr>
-               <td colspan="" width="190px" height="20px" style="font-size: 14px; ">
+               <td colspan="" width="190px" height="30px" style="font-size: 14px; ">
                <?php 
                   if($rvr_reason_for_visit == 'Transfer of Care'){
                     echo '<b>X</b>';
@@ -225,7 +225,7 @@
                   
                   ?>
                </td>
-               <td colspan="" width="285px" height="15px" valign="top" style="font-size: 14px; ">
+               <td colspan="" width="285px" height="30px" valign="top" style="font-size: 14px; ">
                <?php 
                   if($rvr_reason_for_visit == 'Other Reason'){
                     echo '<b>X</b>';
